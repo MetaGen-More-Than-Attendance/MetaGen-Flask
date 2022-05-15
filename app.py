79583747279b83359e64d14e3c82064433e67731
stoylan <toylansamet@gmail.com>
2022-05-16 01:40:03 +0300

    remove unnecessary lines

--- app.py
+++ app.py
@@ -12,12 +12,6 @@ CORS(app)
 
 
 # Create arrays of known face encodings and their names
-
-known_face_names = [
-    "Teoman",
-    "Samet",
-    "Hakan"
-]
 # Initialize some variables
 face_locations = []
 face_encodings = []
@@ -32,26 +26,19 @@ def gen_frames(studentPhoto,takingPhoto):
     # Only process every other frame of video to save time
 
     # Find all the faces and face encodings in the current frame of video
-    known_face_names = ["Samet Toylan"]
     studentPhotoEncoding = face_recognition.face_encodings(studentPhoto)[0]
 
     face_locations = face_recognition.face_locations(takingPhoto)
     face_encodings = face_recognition.face_encodings(takingPhoto, face_locations)
-    pil_image = Image.fromarray(takingPhoto)
-    draw = ImageDraw.Draw(pil_image)
     known_face_encodings = studentPhotoEncoding
 
     matches = face_recognition.compare_faces(known_face_encodings, face_encodings)
     #Instead, use face_distance to calculate similarities
     face_distances = face_recognition.face_distance(known_face_encodings, face_encodings)
     best_match_index = np.argmin(face_distances)
-    name = "Unknown"
     if matches[best_match_index]:
-        name = known_face_names[best_match_index]
-        print(name)
         return 'true'
     # Draw a box around the face using the Pillow module
-    print(name)
     return 'false'
 
 @app.route('/', methods=['POST', 'GET'])
